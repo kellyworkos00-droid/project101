a967dd67c0e3af65388b6bf4b015520e6604db04
kellyworkos <kellyworkos00@gmail.com>
2025-11-28 17:07:00 +0300

    Add animated loader component and CSS

---
+++ src/components/Loader.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../../styles/loader.css";
+
+export default function Loader() {
+  return (
+    <div className="flex items-center justify-center min-h-[100px]">
+      <div className="loader" />
+    </div>
+  );
+}
